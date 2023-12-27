83d43eec8b598c13e3497678434ce529549cbf2b
LauraPastor <laurapastorj@gmail.com>
2023-12-27 13:12:03 +0100

    first commit

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,8 @@
+import * as z from "zod";
+
+export const SignupValidation = z.object({
+    name: z.string().min(2, {message: 'A real one, please'}).max(20),
+    username: z.string().min(2, {message: 'Too short for a cool nickname' }).max(20),
+    email: z.string().email(),
+    password: z.string().min(8, {message: 'Too short' }),
+  });
