8ab52c5e1585c09a16e6e892e03249170f094666
LauraPastor <laurapastorj@gmail.com>
2023-12-28 15:03:08 +0100

    Add complete signup flow

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -4,5 +4,5 @@ export const SignupValidation = z.object({
     name: z.string().min(2, {message: 'A real one, please'}).max(20),
     username: z.string().min(2, {message: 'Too short for a cool nickname' }).max(20),
     email: z.string().email(),
-    password: z.string().min(8, {message: 'Too short' }),
+    password: z.string().min(8, {message: 'At least 8 characters' }),
   });
