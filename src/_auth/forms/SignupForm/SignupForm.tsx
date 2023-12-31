966135775ea8d66845f3f4d6c568c9b35b0bf6a2
LauraPastor <laurapastorj@gmail.com>
2023-12-31 11:50:06 +0100

    Fix typo and force useEffect stay in singup

--- src/_auth/forms/SignupForm/SignupForm.tsx
+++ src/_auth/forms/SignupForm/SignupForm.tsx
@@ -20,7 +20,7 @@ import { useUserContext } from "@/context/AuthContext";
 
 const SignupForm = () => {
   const { toast } = useToast()
-  const { checkAuthUser, isLoading: isUserLoaging } = useUserContext();
+  const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
   const navigate = useNavigate();
 
   const { mutateAsync: createUserAccount, isPending: isCreatingAccount } = useCreateUserAccount();
