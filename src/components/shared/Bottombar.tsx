848beb6205eb09e8bb502d6ef0c4d82a0ab2d9a0
LauraPastor <laurapastorj@gmail.com>
2024-01-02 13:43:28 +0100

    Fix Bottombar error

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -9,7 +9,7 @@ const Bottombar = () => {
             {bottombarLinks.map((link) => {
                 const isActive = pathname === link.route
                 return (
-                    <Link to={link.route} className={`${isActive && 'bg-primary-500 rounded-[10px]'} flex-center flex-col gap-1 p-2 transition`}>
+                    <Link key={`bottombar-${link.label}`} to={link.route} className={`${isActive && 'bg-primary-500 rounded-[10px]'} flex-center flex-col gap-1 p-2 transition`}>
                         <img src={link.imgURL} alt={link.label} width={16} height={16} className={`${isActive && 'invert-white'}`} />
                         <p className='tiny-medium text-light-2'>{link.label}</p>
                     </Link>
